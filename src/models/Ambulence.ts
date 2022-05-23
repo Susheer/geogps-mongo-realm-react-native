ba4ea8aebf80bf4bbe229225afb34e872a45ad18
sudheer <sudheergupta0508@gmail.com>
2022-05-24 00:00:35 +0530

    type is defined

--- src/models/Ambulence.ts
+++ src/models/Ambulence.ts
@@ -4,31 +4,38 @@ export interface Location {
   coordinates: [number];
 }
 
-export class Ambulence extends Realm.Object {
-  _id!: Realm.BSON.ObjectId;
-  name!: string;
-  business_id!: number;
+// export class Ambulence extends Realm.Object {
+//   _id!: Realm.BSON.ObjectId;
+//   name!: string;
+//   business_id!: number;
+//   location: Location;
+
+//   // the Task.generate() method creates Task objects with fields with default values
+//   static generate(name: string, business_id: number, location: Location) {
+//     return {
+//       _id: new Realm.BSON.ObjectId(),
+//       business_id,
+//       location,
+//       name,
+//     };
+//   }
+
+//   // To use a class as a Realm object type, define the object schema on the static property "schema".
+//   static schema = {
+//     name: 'Ambulence',
+//     primaryKey: '_id',
+//     properties: {
+//       _id: 'objectId',
+//       name: 'string',
+//       business_id: 'number',
+//       location: 'object',
+//     },
+//   };
+// }
+
+export interface Ambulence {
+  _id: string;
+  business_id: number;
+  name: string;
   location: Location;
-
-  // the Task.generate() method creates Task objects with fields with default values
-  static generate(name: string, business_id: number, location: Location) {
-    return {
-      _id: new Realm.BSON.ObjectId(),
-      business_id,
-      location,
-      name,
-    };
-  }
-
-  // To use a class as a Realm object type, define the object schema on the static property "schema".
-  static schema = {
-    name: 'Ambulence',
-    primaryKey: '_id',
-    properties: {
-      _id: 'objectId',
-      name: 'string',
-      business_id: 'number',
-      location: 'object',
-    },
-  };
 }
